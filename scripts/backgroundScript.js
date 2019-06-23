f7e5fa1662d705f7d5dac133a4347321ae59b083
Derrick Thornton <derrickguitarmaster@gmail.com>
2019-06-23 21:05:09 +0100

    Added backgroundgenerator webpage, and also links back to the index page and to my github account.

---
+++ scripts/backgroundScript.js
@@ -0,0 +1,21 @@
+let colorOne, colorTwo, input, backgroundColorInfo, backgroundTextNode;
+
+colorOne = document.querySelector('.colorOne');
+colorTwo = document.querySelector('.colorTwo');
+input = document.querySelector('input');
+backgroundColorInfo = document.querySelector('h3');
+backgroundTextNode = document.createTextNode(`${colorOne.value} & ${colorTwo.value}`)
+backgroundColorInfo.appendChild(backgroundTextNode);
+
+const changeColor = () => {
+	document.body.style = `background: linear-gradient(to right, ${colorOne.value}, ${colorTwo.value})`;
+	backgroundColorInfo.removeChild(backgroundTextNode);
+	backgroundColorInfo.appendChild(checkColor());
+}
+const checkColor = () => {
+	backgroundTextNode = document.createTextNode(`${colorOne.value} & ${colorTwo.value}`)
+	return backgroundTextNode;
+}
+
+colorOne.addEventListener('input', changeColor);
+colorTwo.addEventListener('input', changeColor);
